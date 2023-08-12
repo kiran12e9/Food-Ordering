0cc65bb69b34903e35dd1b07efe3f61324520df9
kiran12e9 <r.ravikiranreddy2409@gmail.com>
2023-08-12 20:34:04 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,4 @@
+
+    const division=React.createElement("div",{id:"div1"},[React.createElement("h1",{},"hello "),React.createElement("h1",{},"world ")])
+    const root=ReactDOM.createRoot(document.getElementById("root"))
+    root.render(division)
